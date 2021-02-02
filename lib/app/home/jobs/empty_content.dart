e1d43abd46fba211a18eace3e0ee5156125783b6
ZavbakGit <f%jtVPW3hbmjy!>
2021-02-02 21:14:52 +1000

    finished 313

--- lib/app/home/jobs/empty_content.dart
+++ lib/app/home/jobs/empty_content.dart
@@ -11,17 +11,20 @@ class EmptyContent extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        Text(
-          title,
-          style: TextStyle(fontSize: 32.0, color: Colors.black54),
-        ),
-        Text(
-          message,
-          style: TextStyle(fontSize: 16.0, color: Colors.black54),
-        ),
-      ],
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text(
+            title,
+            style: TextStyle(fontSize: 32.0, color: Colors.black54),
+          ),
+          Text(
+            message,
+            style: TextStyle(fontSize: 16.0, color: Colors.black54),
+          ),
+        ],
+      ),
     );
   }
 }
--- lib/app/home/jobs/jobs_page.dart
+++ lib/app/home/jobs/jobs_page.dart
@@ -4,6 +4,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:time_tracker_flutter_course/app/home/jobs/edit_job_page.dart';
+import 'package:time_tracker_flutter_course/app/home/jobs/empty_content.dart';
 import 'package:time_tracker_flutter_course/app/home/jobs/job_list_tile.dart';
 import 'package:time_tracker_flutter_course/common_widgets/show_alert_dialog.dart';
 import 'package:time_tracker_flutter_course/common_widgets/show_exception_alert_dialog.dart';
@@ -66,15 +67,17 @@ class JobsPage extends StatelessWidget {
       builder: (context, snapshot) {
         if (snapshot.hasData) {
           final jobs = snapshot.data;
-          final children = jobs
-              .map((job) => JobListTile(
-                    job: job,
-                    onTap: () => EditJobPage.show(context,job: job),
-                  ))
-              .toList();
-          return ListView(
-            children: children,
-          );
+
+          if (jobs.isNotEmpty) {
+            final children = jobs
+                .map((job) => JobListTile(
+                      job: job,
+                      onTap: () => EditJobPage.show(context, job: job),
+                    ))
+                .toList();
+            return ListView(children: children);
+          }
+          return EmptyContent();
         }
         if (snapshot.hasError) {
           return Center(child: Text('Some error occurred'));
