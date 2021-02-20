d59b4c77c80f2ff72e9e18d71d6f0306a59f1686
ZavbakGit <f%jtVPW3hbmjy!>
2021-02-20 17:17:03 +1000

    finished

--- test/email_sign_in_bloc_test.dart
+++ test/email_sign_in_bloc_test.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
 import 'package:time_tracker_flutter_course/app/sign_in/email_sign_in_bloc.dart';
 import 'package:time_tracker_flutter_course/app/sign_in/email_sign_in_model.dart';
 
@@ -17,16 +19,44 @@ void main() {
     bloc.dispose();
   });
 
-  test('WHEN email is updated'
-  'AND password is updated'
-  'AND submit is called'
-  'THEN modelStream emits the correct events', () async {
+  test(
+      'WHEN email is updated'
+          'AND password is updated'
+          'AND submit is called'
+          'THEN modelStream emits the correct events', () async {
+    when(mockAuth.signInWithEmailAndPassword(any, any))
+        .thenThrow(PlatformException(code: 'ERROR'));
+    expect(
+        bloc.modelStream,
+        emitsInOrder([
+          EmailSignInModel(),
+          EmailSignInModel(email: 'email@email.com'),
+          EmailSignInModel(
+            email: 'email@email.com',
+            password: 'password',
+          ),
+          EmailSignInModel(
+            email: 'email@email.com',
+            password: 'password',
+            submitted: true,
+            isLoading: true,
+          ),
+          EmailSignInModel(
+            email: 'email@email.com',
+            password: 'password',
+            submitted: true,
+            isLoading: false,
+          ),
+        ])
+    );
 
-    final Stream<EmailSignInModel> modelStream = bloc.modelStream;
-    modelStream.listen((event) {
-      print(event);
-    });
+    bloc.updateEmail('email@email.com');
 
-    expect(modelStream, emits(EmailSignInModel()));
+    bloc.updatePassword('password');
+
+    try {
+      await bloc.submit();
+    } catch (_) {
+    }
   });
 }
